31c6e89d3163ded05b6b5ebd7c85930f5e1efee4
Chin Wei <chinwei.w@gmail.com>
2014-07-21 16:55:11 -0400

    export html

--- html/js/script.js
+++ html/js/script.js
@@ -1,10 +1,33 @@
 var App = {
 	init: function(){
-		Pizza.init();
+		paymentComponent.init();
 	}
 }
 
 
-$(window).load(function() {
-  App.init();
-});
+
+var paymentComponent = {
+	init: function() {
+		paymentComponent.handleEvent();
+	},
+	handleEvent: function() {
+		$('#make-payment-btn').click(paymentComponent.showPaymentComponent);
+		$('#make-payment-btn').click(paymentComponent.disableButton);
+		$('#cancel-payment-btn').click(paymentComponent.hidePaymentComponent);
+		$('#cancel-payment-btn').click(paymentComponent.enableButton);
+	},
+	showPaymentComponent: function(){
+		$('#pay-now').removeClass('hide');
+	},
+	hidePaymentComponent: function(){
+		$('#pay-now').addClass('hide');
+	},
+	disableButton: function(){
+		$('#make-payment-btn').addClass('disabled');
+	},
+	enableButton: function(){
+		$('#make-payment-btn').removeClass('disabled');
+	}
+}
+
+App.init();
